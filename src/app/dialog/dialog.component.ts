291b4462a64bf2ccb49f36e0992926b1df64d973
olddonny <reedtrekks@gmail.com>
2017-11-27 16:12:58 -0600

    login service

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Inject } from '@angular/core';
 import {MatDialogModule, MAT_DIALOG_DATA,MatDialogRef} from '@angular/material';
 import { ListComponent } from '../items/list/list.component';
+import {ChirpsService} from '../services/chirps/chirps.service'
 
 @Component({
   selector: 'app-dialog',
@@ -8,12 +9,26 @@ import { ListComponent } from '../items/list/list.component';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent {
+  chirps: any
   constructor(
+    public svc : ChirpsService,
     public dialogRef: MatDialogRef<ListComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  refresh(): void {
+    window.location.reload();
+  }
+
+
+  deleteChirp(){
+    this.svc.deleteChirp(this.data.id)
+    .subscribe(()=> {
+    
+    })
   }
 
 }
--- src/app/forms/signinform/signinform.component.ts
+++ src/app/forms/signinform/signinform.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 import { FormsModule, FormBuilder, Validators, FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { LoginService } from '../../services/login/login.service';
 
 
 
@@ -15,7 +16,8 @@ export class SigninformComponent  {
   
  
    constructor(@Inject(MAT_DIALOG_DATA) private data: any, 
-                private fb: FormBuilder){
+                private fb: FormBuilder,
+                private loginsvc: LoginService){
 
      this.loginForm = this.fb.group({
        
@@ -25,5 +27,18 @@ export class SigninformComponent  {
      });
     
  }
+
+ login(username:string, password:string): void  {
+   let thislogin={ username, password}
+ if(this.loginForm.valid){
+   
+   this.loginsvc.login(thislogin)
+   .subscribe(Responce =>{
+     
+   })
+ }
+}
+
+ 
 
 }
